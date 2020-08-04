e0df34677552b226e39a726aa7549c6ccaa171ce
Abdallah Al-Barmawi <abarmawi@live.com>
2020-08-04 22:45:17 +0200

    remove socket service

--- src/modules/board/views/board/BoardPage.vue
+++ src/modules/board/views/board/BoardPage.vue
@@ -8,31 +8,13 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import Board from './components/Board.vue'
-import { GameService, WebSocketClient } from '@/biz/services/GameService'
-import { ChessMove } from './ChessBoardSetup'
 
 @Component({
   components: {
     Board,
   },
 })
-export default class BoardPage extends Vue implements WebSocketClient {
-  loading = true
-  gameService!: GameService
-
-  created() {
-    this.loading = false
-    this.gameService = new GameService(this)
-  }
-
-  socketReady() {
-    this.gameService.newGame()
-  }
-
-  move(move: ChessMove) {
-    this.gameService.move(move)
-  }
-}
+export default class BoardPage extends Vue {}
 </script>
 
 <style lang="scss" scoped></style>
